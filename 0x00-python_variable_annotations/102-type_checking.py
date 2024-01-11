5c8b65b885e1793ab651c1ce0f4c86c18df5b748
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-11 09:54:09 +0000

    Fix mypy errors in a given piece of code

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Type Checking
+
+Functions:
+    zoom_array(lst, factor)
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Zooms into a tuple by increasing the count of each element to factor.
+
+    Args:
+        lst: The tuple
+        factor: The factor by which to zoom in. Default is 2.
+
+    Returns:
+        A list of the zoomed in tuple.
+    """
+
+    zoomed_in: List = [
+            item for item in lst
+            for i in range(factor)
+            ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
--- 0x00-python_variable_annotations/README.md
+++ 0x00-python_variable_annotations/README.md
@@ -63,3 +63,21 @@ def safely_get_value(dct, key, default = None):
     else:
         return default
 ```
+
+### Task 12
+Use `mypy` to validate the following piece of code and apply any necessary changes.
+```
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
+```
