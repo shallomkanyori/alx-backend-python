0ca99fe37bf3f5d60f331c6d3538f0911ced7fba
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-15 08:57:48 +0000

    Add async function that waits for random delay
    Add an async Python coroutine that takes in an integer argument
    max_delay, waits for a random delay between 0 and max_delay and
    eventually returns it.

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Async Basics
+
+Functions:
+    wait_random(max_delay)
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    """Asynchronously waits for a random delay then returns the delay.
+
+    Args:
+        max_delay: The maximum delay. The function waits for a random delay
+                   between 0 and max_delay.
+    """
+
+    wait_time = random.uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+
+    return wait_time
--- 0x01-python_async_function/README.md
+++ 0x01-python_async_function/README.md
@@ -1,1 +1,8 @@
 # Python - Async
+
+## Tasks
+
+### Task 0
+Write an asynchronous coroutine that takes in an integer argument (`max_delay`, with a default value of 10) named `wait_random` that waits for a random delay between 0 and `max_delay` (included and float value) seconds and eventually returns it.
+
+Use the `random` module.
