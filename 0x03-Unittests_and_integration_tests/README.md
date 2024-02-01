0f15bd9725cd899cffe815fece9224f8e9166d5c
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-01 15:56:17 +0000

    Implement unittest that mocks HTTP calls
    Implement a unittest for the get_json method that mocks HTTP calls.

--- 0x03-Unittests_and_integration_tests/README.md
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -28,3 +28,16 @@ nested_map={}, path=("a",)
 nested_map={"a": 1}, path=("a", "b")
 ```
 Also make sure that the exception message is as expected.
+
+### Task 2
+File: [test_utils.py](test_utils.py)
+Define the `TestGetJson`(`unittest.TestCase`) class and implement the `TestGetJson.test_get_json` method to test that `utils.get_json` returns the expected result.
+
+Use `unittest.mock.patch` to patch `requests.get`. Make sure it returns a `Mock` object with a `json` method that returns `test_payload` which you parametrize alongside the `test_url` that you will pass to `get_json` with the following inputs:
+```
+test_url="http://example.com", test_payload={"payload": True}
+test_url="http://holberton.io", test_payload={"payload": False}
+```
+
+Test that the mocked `get` method was called exactly once (per input) with `test_url` as argument.
+Test that the output of `get_json` is equal to `test_payload`.
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-"""Tests the utils.access_nested_map method
+"""Tests the utils module
 
 Classes:
     TestAccessNestedMap
+    TestGetJson
 """
 import unittest
+import requests
+from unittest import mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import (
+        access_nested_map,
+        get_json
+)
 from typing import Mapping, Sequence, Any
 
 
@@ -32,3 +38,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests exceptions from the access_nested_map method"""
         with self.assertRaises(KeyError, msg=expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests the get_json method"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url: str, test_payload: Mapping):
+        """Tests the get_json method"""
+
+        with mock.patch('requests.get') as mock_get:
+            mock_resp = mock.Mock(**{
+                'json.return_value': test_payload
+            })
+            mock_get.return_value = mock_resp
+
+            output = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            assert output == test_payload
