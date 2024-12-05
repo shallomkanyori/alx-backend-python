308f8d0f0b2e7676515d4e32630ddd0a1b7524e3
shallomkanyori <shallomkanyori@gmail.com>
2024-12-05 18:32:30 +0300

    Add generator that gets and processes data in batches

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,23 @@
+"""This modules creates a generator to fetch and process data in batches from the users database"""
+import seed
+
+def stream_users_in_batches(batch_size):
+    """Fetches rows in batches"""
+
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM user_data;")
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    for i in range(0, len(rows), batch_size):
+        yield rows[i:i + batch_size]
+
+def batch_processing(batch_size):
+    """Processes each batch to filter users over the age of 25"""
+
+    for batch in stream_users_in_batches(batch_size):
+        for row in batch:
+            if row[3] > 25:
+                print(row)
--- python-generators-0x00/README.md
+++ python-generators-0x00/README.md
@@ -1,8 +1,11 @@
 # Python Generators
 
-**Objective:** create a generator that streams rows from an SQL database one by one.
+**Objective:** Create a generator that streams rows from an SQL database one by one.
 ## Task 1
 Set up the database and seed the table
 
 ## Task 2
 Create the generator.
+
+## Task 3
+**Objective:** Create a generator to fetch and process data in batches from the users database.
