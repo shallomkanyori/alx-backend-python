111189aefdf3cc5842f730cc452fff02f43bd861
shallomkanyori <shallomkanyori@gmail.com>
2024-12-21 12:55:17 +0300

    Change import for DefaultRouter

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
 from .views import ConversationViewSet, MessageViewSet
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from django.urls import path, include
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 router.register(r'messages', MessageViewSet, basename='messages')
 
