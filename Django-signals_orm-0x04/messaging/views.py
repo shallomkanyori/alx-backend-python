ff835347a27aa0bdd640853fadc576e543a38108
shallomkanyori <shallomkanyori@gmail.com>
2025-01-12 07:45:42 +0300

    Select only necessary fields

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -31,7 +31,7 @@ def get_threaded_replies(message):
 class MessageViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = Message.objects.filter(Q(sender=request.user) | Q(receiver=request.user))
-        unread_messages = Message.unread.unread_for_user(request.user)
+        unread_messages = Message.unread.unread_for_user(request.user).only('content', 'timestamp')
         serializer = MessageSerializer(queryset, many=True)
         unread_serializer = MessageSerializer(unread_messages, many=True)
         return Response({
